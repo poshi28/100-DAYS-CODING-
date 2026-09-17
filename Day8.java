4042ce794d9dbb56dce9ba19373248a4b3252dcf
poshi28 <gdsharma39@gmail.com>
2026-09-17 17:31:35 +0530

    Day-8 Compute Power using Recursion

---
+++ Day8.java
@@ -0,0 +1,33 @@
+/*Problem: Given integers a and b, compute a^b using recursion without using pow() function.
+
+Input:
+- Two space-separated integers a and b
+
+Output:
+- Print a raised to power b
+
+Example:
+Input:
+2 5
+
+Output:
+32
+
+Explanation: 2^5 = 2 * 2 * 2 * 2 * 2 = 32*/
+
+
+import java.util.Scanner;
+public class Day8 {
+    static int power(int a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+        return a * power(a, b - 1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(power(a, b));
+    }
+}
